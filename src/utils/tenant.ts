310d0278510daabc0ff4543d1d7ed21d96431025
Tom <tom@tochito.co.uk>
2025-05-20 15:30:45 +0100

    Update tenant.ts

--- src/utils/tenant.ts
+++ src/utils/tenant.ts
@@ -2,5 +2,11 @@ import { PROTOCOL, ROOT_DOMAIN } from "@/constants";
 
 export function generateTenantRedirectUrl(subdomain: string, path: string) {
   const tenant = subdomain.toLowerCase().replace(/[^a-z0-9-]/g, "");
+  const isPreview = subdomain.includes("---");
+
+  if (isPreview) {
+    return `${PROTOCOL}://${tenant}---${ROOT_DOMAIN}/${path}`;
+  }
+
   return `${PROTOCOL}://${tenant}.${ROOT_DOMAIN}/${path}`;
 }
